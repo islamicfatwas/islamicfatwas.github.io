832b572e1ca731a0c0f8cfd17a95dc429054a5a1
salafmanhaj <aliftaid@gmail.com>
2024-06-18 17:08:34 +0700

    Create ui.ts

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+  en: 'English',
+  ar: 'عربي',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'fatwas': 'About',
+    'audios': 'Audios',
+    'search': 'Search',
+    'langName': 'عربي',
+  },
+  ar: {
+    'fatwas': 'الفتاوى',
+    'audios': 'صوتيات',
+    'search': 'البحث',
+    'langName': 'English',
+  },
+} as const;
