e679f16a02b28c6b911d9410d51b887e0f17f36f
salafmanhaj <aliftaid@gmail.com>
2024-06-18 21:45:07 +0700

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -32,15 +32,23 @@ const blog = defineCollection({
 export const collections = { blog };
 
 const articleCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    author: z.string().default(SITE.author),
-    pubDatetime: z.date(),
-    modDatetime: z.date().optional().nullable(),
-    featured: z.boolean().optional(),
-    draft: z.boolean().optional(),
-    tags: z.array(z.string()).default(["others"]),
-    ogImage: image()
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      author: z.string().default(SITE.author),
+      pubDatetime: z.date(),
+      modDatetime: z.date().optional().nullable(),
+      hijriDate: z.string(),
+      title: z.string(),
+      source: z.string(),
+      muftis: z.string(),
+      featured: z.boolean().optional(),
+      AudioPlayer: z.boolean().optional(),
+      audio: z.string().optional(),
+      reference: z.string().optional(),
+      draft: z.boolean().optional(),
+      tags: z.array(z.string()).default(["others"]),
+      ogImage: image()
         .refine(img => img.width >= 1200 && img.height >= 630, {
           message: "OpenGraph image must be at least 1200 X 630 pixels!",
         })
@@ -48,7 +56,7 @@ const articleCollection = defineCollection({
         .optional(),
       description: z.string(),
       canonicalURL: z.string().optional(),
-  }),
+    }),
 });
 
 export const collection = {
