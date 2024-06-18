0e0b3b8e42ae6e9a78ab7c79950ef37354d181d0
salafmanhaj <aliftaid@gmail.com>
2024-06-18 21:39:48 +0700

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -36,7 +36,19 @@ const articleCollection = defineCollection({
     title: z.string(),
     author: z.string().default(SITE.author),
     pubDatetime: z.date(),
-  })
+    modDatetime: z.date().optional().nullable(),
+    featured: z.boolean().optional(),
+    draft: z.boolean().optional(),
+    tags: z.array(z.string()).default(["others"]),
+    ogImage: image()
+        .refine(img => img.width >= 1200 && img.height >= 630, {
+          message: "OpenGraph image must be at least 1200 X 630 pixels!",
+        })
+        .or(z.string())
+        .optional(),
+      description: z.string(),
+      canonicalURL: z.string().optional(),
+  }),
 });
 
 export const collections = {
