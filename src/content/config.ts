daa7e9f5448cb677204042626c6a78ab4c3d5071
salafmanhaj <aliftaid@gmail.com>
2024-06-20 10:32:48 +0700

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -29,3 +29,23 @@ const blog = defineCollection({
 });
 
 export const collections = { blog };
+
+const audioCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    audioUrl: z.string(),
+    date: z.coerce.date(),
+    cover: z.string().optional(),
+    audio: z.number().optional(),
+    season: z.number().optional(),
+    audioType: z.string().optional(),
+    duration: z.coerce.string(), //duration in format hh:mm:ss
+    size: z.number(), // size in megabytes
+  }),
+})
+
+export const collections = {
+  audio: audioCollection,
+}
