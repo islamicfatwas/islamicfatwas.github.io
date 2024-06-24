d3c40c70a0c8d5ed6c8cfac11a4e946f88cd2667
islamicfatwas <aliftaid@gmail.com>
2024-06-24 16:00:46 +0700

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,23 +6,23 @@ const blog = defineCollection({
   schema: ({ image }) =>
     z.object({
       author: z.string().default(SITE.author),
-      pubDatetime: z.date(),
+      pubDatetime: z.date().optional(),
       modDatetime: z.date().optional().nullable(),
-      hijriDate: z.string(),
-      title: z.string(),
-      source: z.string(),
-      muftis: z.string(),
+      hijriDate: z.string().optional(),
+      title: z.string().optional(),
+      source: z.string().optional(),
+      muftis: z.string().optional(),
       featured: z.boolean().optional(),
       audio: z.string().optional(),
       draft: z.boolean().optional(),
-      tags: z.array(z.string()).default(["others"]),
+      tags: z.array(z.string()).optional().default(["others"]),
       ogImage: image()
         .refine(img => img.width >= 1200 && img.height >= 630, {
           message: "OpenGraph image must be at least 1200 X 630 pixels!",
         })
         .or(z.string())
         .optional(),
-      description: z.string(),
+      description: z.string().optional(),
       canonicalURL: z.string().optional(),
     }),
 });
@@ -32,55 +32,28 @@ const ar = defineCollection({
   schema: ({ image }) =>
     z.object({
       author: z.string().default(SITE.author),
-      pubDatetime: z.date(),
+      pubDatetime: z.date().optional(),
       modDatetime: z.date().optional().nullable(),
-      hijriDate: z.string(),
-      title: z.string(),
-      source: z.string(),
-      muftis: z.string(),
+      hijriDate: z.string().optional(),
+      title: z.string().optional(),
+      source: z.string().optional(),
+      muftis: z.string().optional(),
       featured: z.boolean().optional().default(false),
       audio: z.string().optional(),
       draft: z.boolean().optional(),
-      tags: z.array(z.string()).default(["others"]),
+      tags: z.array(z.string()).optional().default(["others"]),
       ogImage: image()
         .refine(img => img.width >= 1200 && img.height >= 630, {
           message: "OpenGraph image must be at least 1200 X 630 pixels!",
         })
         .or(z.string())
         .optional(),
-      description: z.string(),
-      canonicalURL: z.string().optional(),
-    }),
-});
-
-const audios = defineCollection({
-  type: "content",
-  schema: ({ image }) =>
-    z.object({
-      author: z.string().default(SITE.author),
-      pubDatetime: z.date(),
-      modDatetime: z.date().optional().nullable(),
-      hijriDate: z.string(),
-      title: z.string(),
-      source: z.string(),
-      muftis: z.string(),
-      featured: z.boolean().optional(),
-      audio: z.string().optional(),
-      draft: z.boolean().optional(),
-      tags: z.array(z.string()).default(["others"]),
-      ogImage: image()
-        .refine(img => img.width >= 1200 && img.height >= 630, {
-          message: "OpenGraph image must be at least 1200 X 630 pixels!",
-        })
-        .or(z.string())
-        .optional(),
-      description: z.string(),
+      description: z.string().optional(),
       canonicalURL: z.string().optional(),
     }),
 });
 
 export const collections = {
   blog: blog,
-  audios: audios,
   ar: ar,
 };
